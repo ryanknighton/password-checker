bd3b412bc85660a41ba9ffd762cdd380846f2bff
rknighton <knighton.7@buckeyemail.osu.edu>
2017-11-14 22:37:32 -0500

    Added tooltips
    Tooltips added

--- js/common-criteria.js
+++ js/common-criteria.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
-
+$('[data-toggle="tooltip"]').tooltip(); 
 createCommonCriteria();
 
 function createCommonCriteria(){
 	var websites = ["instagram", "amazon", "dropbox", "linkedin", "google", "github" ,"facebook-official", "snapchat-square", "skype", "youtube", "twitter", "paypal"];
+	var tooltips = ["Instagram passwords must be longer than 5 characters.",
+	"Amazon passwords must be longer than 5 characters.",
+	"Dropbox passwords must be longer than 5 characters.",
+	"LinkedIn passwords must be longer than 5 characters.",
+	"Google passwords must be longer than 7 characters.",
+	"Github passwords must be longer than 6 characters and must contain either lowercase/uppercase letters and at least one number.",
+	"Facebook passwords must be longer than 6 characters and must contain either lowercase/uppercase letters and at least one special character.",
+	"Snapchat passwords must be longer than 7 characters and contain either lower/uppercase letters and numbers or special characters.",
+	"Skype passwords must be between 6 and 20 characters with lower/uppercase letters and at least one number.",
+	"YouTube passwords must be longer than 7 characters.",
+	"Twitter passwords must be longer than 5 characters.",
+	"Paypal passwords must be between 7 and 32 characters with lower/uppercase letters and numbers or special characters."];
 	var list = $('<ul class="criteria-list">');
 	for(var i=0;i<websites.length;i++){
-		list.append('<li><i class="fa fa-'+websites[i]+'" aria-hidden="true"></i></li>');
+		list.append('<li data-toggle="tooltip" title="'+tooltips[i]+'"><i class="fa fa-'+websites[i]+'" aria-hidden="true"></i></li>');
     }
-    list.append('</ul>')
+    list.append('</ul>');
 	$('#password-criteria').html(list);
 }
 
